8ec84d5d6332af30fc3c054b1c1cbffb06a7bfa5
Logan Wright <logan.william.wright@gmail.com>
2016-05-30 19:39:51 -0400

    updating to 0.14

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/sarbogast/vapor.git", majorVersion: 0, minor: 13),
+        .Package(url: "https://github.com/sarbogast/vapor.git", majorVersion: 0, minor: 14),
         .Package(url: "https://github.com/PlanTeam/MongoKitten.git", majorVersion: 0, minor: 10)
     ],
     exclude: [
